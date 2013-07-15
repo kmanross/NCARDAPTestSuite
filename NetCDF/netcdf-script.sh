7233b1151c74397bcb25786032ab2c7f930da055
Kevin L. Manross <manross@ucar.edu>
2013-07-15 15:59:17 -0600

    Added NetCDF scripts and README files

---
+++ NetCDF/netcdf-script.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+# Using NetCDF 4.2 (built Aug 2012) <http://www.unidata.ucar.edu/software/netcdf/>
+# Replace USERNAME and PASSWORD with your credentials making sure to use
+# %40 instead of @ in the username: manross@ucar.edu should be manross%40ucar.edu
+
+ncdump -h http://USERNAME:PASSWORD@castle.ucar.edu/thredds/dodsC/FNLCollection/best
+
